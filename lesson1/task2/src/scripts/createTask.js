120039b089635600cb833bc3fb0fbd3343e4b1b1
Andrey <dvij@iMac.local>
2020-05-26 21:03:38 +0300

    lesson 1 task 2

---
+++ lesson1/task2/src/scripts/createTask.js
@@ -0,0 +1,35 @@
+import {setItem, getItem } from './storage.js'
+import {renderListItems} from './renderListItems.js'
+import {getTasksList, createTask} from './taskGateway.js'
+
+const taskCreateBtn = document.querySelector(".create-task-btn");
+
+const onCreateTask = () => {
+  const taskInput = document.querySelector(".task-input");
+  const text = taskInput.value;
+
+  if (!text) return false;
+  taskInput.value = "";
+
+  const newTask = {
+    text,
+    done: false,
+    createDate: new Date().toISOString(),
+  }
+
+  createTask(newTask)
+    .then(() => getTasksList())
+    .then(newTasksList => {
+      setItem('tasksList', newTasksList);
+      renderListItems();
+    })
+  
+};
+
+taskCreateBtn.addEventListener("click", onCreateTask);
+
+// 1. Prepare data
+// 2. Write data in db
+// 3. Read new data from server
+// 4. Save new data to fron-end storage
+// 5. Update UI based on new data
