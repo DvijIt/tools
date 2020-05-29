92cb95fbcbc668e76b952507c4a14025d95cf995
Andrey <dvij@iMac.local>
2020-05-29 10:01:00 +0300

    lesson 9 task 2 todo-app eslint

---
+++ lesson9/task2/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: 'eslint-config-airbnb-base',
+    rules: {
+        'no-console': 2,
+        // "import/extensions": 0,
+        "import/no-unresolved": 0
+    },
+    env: {
+        browser: true
+    }
+};
---
+++ lesson9/task2/src/index.js
@@ -0,0 +1,22 @@
+import { setItem } from './list/storage';
+import renderListItems from './list/renderListItems';
+import {} from './list/createTask';
+import {} from './list/updateTask';
+import { getTasksList } from './list/taskGateway';
+import {} from './list/deleteTask';
+import './index.scss';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  getTasksList()
+    .then((tasksList) => {
+      setItem('tasksList', tasksList);
+      renderListItems();
+    });
+});
+
+const onStorageChange = (e) => {
+  if (e.key === 'tasksList') renderListItems();
+};
+
+window.addEventListener('storage', onStorageChange);
